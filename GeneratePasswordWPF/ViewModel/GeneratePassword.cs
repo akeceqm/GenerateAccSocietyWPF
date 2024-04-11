20b43c753dd908dff20b88c60262ceafd5aac1a6
akurok <vladkonovalov20060@gmail.com>
2024-04-11 23:38:29 +0500

    Сделал базовый функционал. Сделал дизайн как в проекте с криптой. Еще кучу всего нужно доднелать

---
+++ GeneratePasswordWPF/ViewModel/GeneratePassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePasswordWPF.ViewModel
+{
+    public class GeneratePassword
+    {
+        private Random random = new Random();
+        public string GenerateVariblePasswordLet(List<string> lettersList, int amountPass)
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+            int listSize = lettersList.Count;
+            for (int i = 0; i < amountPass; i++)
+            {
+                int randomIndex = random.Next(listSize);
+                string randomChar = lettersList[randomIndex];
+                passwordBuilder.Append(randomChar);
+            }
+            return passwordBuilder.ToString();
+        }
+
+    }
+}
