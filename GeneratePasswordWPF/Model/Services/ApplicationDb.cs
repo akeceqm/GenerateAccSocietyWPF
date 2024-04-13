989fb1634d5f64157b61fcc7896534978d20026e
akurok <vladkonovalov20060@gmail.com>
2024-04-13 23:36:01 +0500

    Подключил чуть чуть бд. Рабоыт еще много БД для тестов и робит!

--- GeneratePasswordWPF/Model/Services/ApplicationDb.cs
+++ GeneratePasswordWPF/Model/Services/ApplicationDb.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using GeneratePasswordWPF.Model.DbTables;
+using Microsoft.Data.Sqlite;
 
 namespace GeneratePasswordWPF.Model.Services
 {
-    class ApplicationDb
+    public class ApplicationDb
     {
+        public ApplicationDb()
+        {
+            var connection = new SqliteConnection("Data Source=ApplicationDb.db");
+            connection.Open();
+            SqliteCommand command = new SqliteCommand();
+            try
+            {
+                command.Connection = connection;
+                command.CommandText = "CREATE TABLE UserTable(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Login TEXT NOT NULL, Password TEXT NOT NULL)";
+                command.ExecuteNonQuery();
+            }
+            catch { }
+        }
+        private SqliteConnection Conn()
+        {
+            var connection = new SqliteConnection("Data Source=ApplicationDb.db");
+            connection.Open();
+            return connection;
+        }
+
+        public void AddAcc(string Login, string Password)
+        {
+            SqliteCommand command = new SqliteCommand();
+            command.Connection = Conn();
+            command.CommandText = $"INSERT INTO UserTable (Login,Password) VALUES ('{Login}','{Password}')";
+            command.ExecuteNonQuery();
+        }
     }
 }
